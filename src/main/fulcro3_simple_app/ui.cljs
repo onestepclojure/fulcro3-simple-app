c63653057cc95653892c0eec544ad9367ca35f58
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-03 01:15:57 +0530

    ready for v2.0 release after code restructuring

--- src/main/fulcro3_simple_app/ui.cljs
+++ src/main/fulcro3_simple_app/ui.cljs
@@ -3,11 +3,9 @@
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]
     ;; ui components
-    [fulcro3-simple-app.components.person :refer [ui-person]]
     [fulcro3-simple-app.components.fulcrologo :refer [ui-fulcrologo]]))
 
 
 (defsc Root [this props]
   (dom/div
-    (ui-person {:person/name "Joe" :person/age 22})
     (ui-fulcrologo)))
