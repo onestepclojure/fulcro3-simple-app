b9d37a3ab5d7e5c397b0eab54976f429256860a8
Abhinav Sharma <abhi18av@users.noreply.github.com>
2019-08-03 01:45:12 +0530

    Update newFulcroProject.sh

--- scripts/newFulcroProject.sh
+++ scripts/newFulcroProject.sh
@@ -1,5 +1,5 @@
 mkdir fulcro3-simple-app
 cd fulcro3-simple-app
-mkdir -p src/main src/dev resources/public
+mkdir -p src/main src/main/fulcro3_simple_app src/dev resources/public 
 npm init
 npm install shadow-cljs react react-dom --save
