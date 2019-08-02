2edcffd1b8b82e8686a5b085578360c754c01598
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-03 01:13:49 +0530

    split the code for ui and main app

---
+++ src/main/fulcro3_simple_app/application.cljs
@@ -0,0 +1,5 @@
+(ns fulcro3-simple-app.application
+  (:require
+    [com.fulcrologic.fulcro.application :as app]))
+
+(defonce app (app/fulcro-app))
--- src/main/fulcro3_simple_app/client.cljs
+++ src/main/fulcro3_simple_app/client.cljs
@@ -1,27 +1,20 @@
 (ns fulcro3-simple-app.client
   (:require
     [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]
-    ;; app code
-    [fulcro3-simple-app.components.fulcrologo :refer [ui-fulcrologo]]
-    ))
-
-(defonce app (app/fulcro-app))
-
-(defsc Root [this props]
-  (dom/div
-    (ui-fulcrologo)))
+    ;; application code
+    [fulcro3-simple-app.application :refer [app]]
+    ;; ui code
+    [fulcro3-simple-app.ui :as ui]))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
   []
-  (app/mount! app Root "app")
+  (app/mount! app ui/Root "app")
   (js/console.log "Loaded"))
 
 (defn ^:export refresh
   "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
   []
   ;; re-mounting will cause forced UI refresh, update internals, etc.
-  (app/mount! app Root "app")
+  (app/mount! app ui/Root "app")
   (js/console.log "Hot reload"))
